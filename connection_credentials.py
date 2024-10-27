2a2025d9461d1b4eb4736d9462431aa9923d2800
DrewProkarym <Drew.M.Prokarym-1@ou.edu>
2024-10-27 10:07:25 -0500

    Stopped tracking connection_credentials.py

--- connection_credentials.py
+++
@@ -1,7 +0,0 @@
-def get_connection_string():
-    username = ''
-    password = ''
-    host = ''
-    port = ''
-    database = ''
-    return f'postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}'
