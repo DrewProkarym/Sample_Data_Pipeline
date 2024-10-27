af55c17a8c219fc7ff4be197ec25eb632b9839be
DrewProkarym <Drew.M.Prokarym-1@ou.edu>
2024-10-27 09:49:08 -0500

    Stop tracking connection_credentials

--- connection_credentials.py
+++
@@ -1,7 +0,0 @@
-def get_connection_string():
-    username = 'postgres'
-    password = 'Postgres1979!'
-    host = 'localhost'
-    port = '5432'
-    database = 'sample_pipeline_database1'
-    return f'postgresql+psycopg2:/{username}:{password}@{host}:{port}/{database}'
